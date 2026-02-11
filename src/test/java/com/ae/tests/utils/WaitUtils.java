982ab5361088362fd7645e24ca94486c42b4e363
Sala <saladzenika727@gmail.com>
2026-02-12 00:20:32 +0400

    feat(utils): add WaitUtils for explicit waits

---
+++ src/test/java/com/ae/tests/utils/WaitUtils.java
@@ -0,0 +1,23 @@
+package com.ae.tests.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtils {
+
+    // Wait until element is visible
+    public static WebElement waitForVisible(WebDriver driver, WebElement element) {
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOf(element));
+    }
+
+    // Wait until element is clickable
+    public static WebElement waitForClickable(WebDriver driver, WebElement element) {
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(element));
+    }
+}
